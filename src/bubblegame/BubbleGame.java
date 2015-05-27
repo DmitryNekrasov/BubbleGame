f3d8318297dbbba3ab4eb5de7eba65e20953e1f5
DmitryNekrasov <scannorone@gmail.com>
2015-05-27 19:23:00 +0600

    + главная форма

--- src/bubblegame/BubbleGame.java
+++ src/bubblegame/BubbleGame.java
@@ -15,7 +15,8 @@ public class BubbleGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainForm mainForm = new MainForm();
+        mainForm.setVisible(true);
     }
     
 }
